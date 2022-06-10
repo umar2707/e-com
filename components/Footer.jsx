15920c2dcf0da224e4aa22f6e7c1a3fef0477f4b
umar2707 <nurbekatajonov67@gmail.com>
2022-06-10 22:28:00 +0500

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {AiFillInstagram} from 'react-icons/ai'
+
+const Footer = () => {
+  return (
+    <div  className='footer-container'>
+      <p>2022 JSM headphones all rights reserved</p>
+      <p>
+        <AiFillInstagram />
+      </p>
+    </div>
+  )
+}
+
+export default Footer
